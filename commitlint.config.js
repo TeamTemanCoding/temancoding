4b721faeb3a7b209082cf1d3ccbc5ab372f66a5d
iqblfrdsyh <iqblfrdsyh@gmail.com>
2025-03-23 20:17:10 +0700

    feat(setup): husky

---
+++ commitlint.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    extends: ["@commitlint/config-conventional"],
+    rules: {
+      "type-enum": [
+        2,
+        "always",
+        [
+          "build",
+          "chore",
+          "docs",
+          "feat",
+          "fix",
+          "perf",
+          "ops",
+          "refactor",
+          "revert",
+          "test",
+        ],
+      ],
+    },
+  };
+  
