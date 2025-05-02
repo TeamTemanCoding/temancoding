81afbd94d7f5c798b13efa0d9b333d6da4c07ecf
iqblfrdsyh <iqblfrdsyh@gmail.com>
2025-05-02 18:59:28 +0700

    style(portofolio): fix mobile style

--- src/components/section/BenefitSection.tsx
+++ src/components/section/BenefitSection.tsx
@@ -7,9 +7,9 @@ const BenefitSection = () => {
     return (
         <SectionContainer className='h-full mt-20 sm:-mt-0 sm:h-[700px]' id='benefits' container>
             <div className='flex items-center justify-center sm:max-w-[1300px] sm:mx-auto sm:gap-16 h-full relative'>
-                <Image src={"./images/benefits.svg"} alt='benefit' width={550} height={550} draggable={false} className='w-[650px] sm:w-[550px] hidden sm:block' />
+                <Image src={"./images/benefits.svg"} alt='benefit' width={550} height={550} draggable={false} className='w-[650px] sm:w-[550px] hidden lg:block' />
                 <div className='h-auto -mt-16 sm:-mt-0'>
-                    <h2 className='text-[32px] sm:text-[40px] text-center sm:text-left font-bold z-50'>Why Choose <span className='text-[#5B86E5]'>Us?</span></h2>
+                    <h2 className='text-[32px] sm:text-[40px] text-center lg:text-left font-bold z-50'>Why Choose <span className='text-[#5B86E5]'>Us?</span></h2>
                     <div className='overflow-hidden mt-8 sm:mt-8'>
                         <div className='h-fit bg-[#042246] rounded-[17px] w-auto sm:w-[600px] px-5 py-2'>
                             <CompAccordion />
--- src/components/ui/Portofolio.tsx
+++ src/components/ui/Portofolio.tsx
@@ -5,19 +5,19 @@ import React from 'react'
 
 const Portofolio = () => {
     return (
-        <div className=' max-w-[1000px] place-items-center'>
-            <div className="bg-[#042246] p-5 rounded-[9px] w-[250px] grid grid-cols-1 sm:grid-cols-2 sm:bg-transparent sm:w-full sm:rounded-none gap-2 sm:gap-10">
-                <Image src={"/images/portofolio/portofolio1.png"} alt='1' width={485} height={485} className='mb-5 sm:mb-0' />
+        <div className='max-w-[1000px] place-items-center mx-auto'>
+            <div className="bg-[#042246] p-5 rounded-[9px] w-[250px]  grid grid-cols-1 lg:grid-cols-2 lg:bg-transparent lg:w-full lg:rounded-none gap-1 mx-auto lg:gap-10">
+                <Image src={"/images/portofolio/portofolio1.png"} alt='1' width={485} height={485} className='mb-5 lg:mb-0' />
                 <div className='text-left flex flex-col gap-5'>
-                    <h4 className='text-[20px] sm:text-[38px] font-semibold'>Neonest.id Website</h4>
+                    <h4 className='text-[20px] lg:text-[38px] font-semibold'>Neonest.id Website</h4>
                     <div className='flex items-center gap-3'>
-                        <Image src={"./images/icons/nextjs.svg"} alt='nextjs' width={38} height={38} className='w-[28px] h-[28px] sm:w-[38px] sm:h-[38px]' />
-                        <Image src={"./images/icons/typescript.svg"} alt='nextjs' width={38} height={38} className='w-[28px] h-[28px] sm:w-[38px] sm:h-[38px]' />
-                        <Image src={"./images/icons/tailwind.svg"} alt='nextjs' width={38} height={38} className='w-[28px] h-[28px] sm:w-[38px] sm:h-[38px]' />
+                        <Image src={"./images/icons/nextjs.svg"} alt='nextjs' width={38} height={38} className='w-[28px] h-[28px] lg:w-[38px] lg:h-[38px]' />
+                        <Image src={"./images/icons/typescript.svg"} alt='nextjs' width={38} height={38} className='w-[28px] h-[28px] lg:w-[38px] lg:h-[38px]' />
+                        <Image src={"./images/icons/tailwind.svg"} alt='nextjs' width={38} height={38} className='w-[28px] h-[28px] lg:w-[38px] lg:h-[38px]' />
                     </div>
-                    <p className='max-w-[450px] text-[12px] sm:text-[20px] font-thin'>NeoNest provides professional web development, creative design, and social media boosting services to help your brand grow and stand out online.</p>
+                    <p className='max-w-[450px] text-[12px] lg:text-[20px] font-thin'>NeoNest provides professional web development, creative design, and social media boosting services to help your brand grow and stand out online.</p>
                     <div className='mt-2'>
-                        <Button className='rounded-full w-[120px] sm:w-[140px] h-[38px] bg-white font-medium '>Live Demo <CaretRight weight='bold' className='shrink-0' /> </Button>
+                        <Button className='rounded-full w-[120px] lg:w-[140px] h-[38px] bg-white font-medium '>Live Demo <CaretRight weight='bold' className='shrink-0' /> </Button>
                     </div>
                 </div>
             </div>
